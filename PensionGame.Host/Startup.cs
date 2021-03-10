4f2994204761f40c24c26862417e7a62cc0d4eaa
Vladimír Bača <vladimir.baca@thermofisher.com>
2021-03-10 22:50:26 +0100

    setting up CI

--- PensionGame.Host/Startup.cs
+++ PensionGame.Host/Startup.cs
@@ -49,7 +49,7 @@ namespace PensionGame.Host
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            //if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
