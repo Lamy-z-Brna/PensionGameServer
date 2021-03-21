1a2512675c6281a42e658eb0132f21939971ff61
VladoBaca <asianiajja@gmail.com>
2021-03-21 16:18:57 +0100

    build worflow fixes

--- PensionGame.Web/Client/RestConnectionConfiguration.cs
+++ PensionGame.Web/Client/RestConnectionConfiguration.cs
@@ -4,7 +4,7 @@ namespace PensionGame.Web.Client
 {
     public class RestConnectionConfiguration : IRestConnectionConfiguration
     {
-        private Uri _restApiUri => new("https://pensiongameserver.azurewebsites.net/api/");
+        private Uri _restApiUri => new("https://pensiongameapi.azurewebsites.net/api/");
         public Uri RestApiUri => _restApiUri;
     }
 }
