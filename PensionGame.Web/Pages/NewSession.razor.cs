bf3ad4254ca2bc19eb8f9c60d271ae2a6cfd3f42
VladoBaca <asianiajja@gmail.com>
2021-04-11 18:23:56 +0200

    minor improvements

--- PensionGame.Web/Pages/NewSession.razor.cs
+++ PensionGame.Web/Pages/NewSession.razor.cs
@@ -25,16 +25,16 @@ namespace PensionGame.Web.Pages
 
         private static string RandName()
         {
-            string[] names_1 = new string[] { "Shymo", "Anička", "Adam", "Bači", "Miki", "Matovič", "Sulík", "Ciťo", "Peťo 2D", "Peťo 3D", "Donald Trump", "Joe Biden", "Babiš", "Your mom" };
-            string[] names_2 = new string[] { "is", "is best in", "has", "took a photo of", "found a photo of", "doesn't have", "fixed", "created", "found", "knows nothing about", "waits for", "saves for", "wants", "lost", "won", "won 6 times", "is looking for", "looks like", "caught", "cares about", "doesn't give a fuck about", "would like" };
-            string[] names_3 = new string[] { "a huge", "an expensive", "a dirty", "a cheap", "a fancy", "a godlike", "a tiny", "an overpriced", "a bad", "a sleepy", "a malicious", "a stolen", "a massive", "your" };
-            string[] names_4 = new string[] { "vaccine", "vagina", "pension", "Sputnik", "covid", "dick", "bitch", "pussy", "election", "election result", "election fraud", "joke", "result", "mom" };
+            string[] names_1 = new string[] { "Shymo", "Anička", "Adam", "Bači", "Miki", "Matovič", "Sulík", "Ciťo", "Peťo 2D", "Peťo 3D", "Donald Trump", "Joe Biden", "Babiš", "Your mom", "A scientist", "An epidemologist", "My boss" };
+            string[] names_2 = new string[] { "is", "is best in", "is looking forward to", "is cooking up", "has", "took a photo of", "found a photo of", "doesn't have", "fixed", "created", "found", "knows nothing about", "predicts", "issued a warning about", "waits for", "saves for", "wants", "lost", "won", "won 6 times", "is looking for", "looks like", "caught", "cares about", "doesn't give a fuck about", "would like" };
+            string[] names_3 = new string[] { "a huge", "an expensive", "a dirty", "a toy", "a cheap", "a fancy", "a godlike", "a tiny", "an overpriced", "a bad", "a sleepy", "a malicious", "a stolen", "a massive", "your" };
+            string[] names_4 = new string[] { "vaccine", "vagina", "pension", "Sputnik", "covid", "something", "dick", "bitch", "pussy", "election", "election result", "election fraud", "joke", "result", "mom", "victory", "loss" };
 
-            Random rand = new Random(DateTime.Now.Second);
-            var name_1 = names_1[rand.Next(0, names_1.Length - 1)];
-            var name_2 = names_2[rand.Next(0, names_2.Length - 1)];
-            var name_3 = names_3[rand.Next(0, names_3.Length - 1)];
-            var name_4 = names_4[rand.Next(0, names_4.Length - 1)];
+            Random rand = new Random();
+            var name_1 = names_1[rand.Next(0, names_1.Length)];
+            var name_2 = names_2[rand.Next(0, names_2.Length)];
+            var name_3 = names_3[rand.Next(0, names_3.Length)];
+            var name_4 = names_4[rand.Next(0, names_4.Length)];
 
             return $"{name_1} {name_2} {name_3} {name_4}";
         }
