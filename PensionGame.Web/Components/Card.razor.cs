92212514238560bd13483113bb3aee05bd300440
Anna Nagyova <ann.nagyova@gmail.com>
2021-05-22 13:38:16 +0200

    added Card component

---
+++ PensionGame.Web/Components/Card.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace PensionGame.Web.Components
+{
+    public partial class Card
+    {
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+    }
+}
