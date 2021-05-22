0941a1be5391f509472c6d5c999e2a4fbe8ec685
Anna Nagyova <ann.nagyova@gmail.com>
2021-05-22 15:20:34 +0200

    added ExpandableSection component

---
+++ PensionGame.Web/Components/ExpandableSection.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PensionGame.Web.Components
+{
+    public partial class ExpandableSection
+    {
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+        //[Parameter]
+        public bool Collapsed { get; set; } = true;
+
+        void Toggle()
+        {
+            Collapsed = !Collapsed;
+        }
+    }
+}
