f55a4cf3ad428c63ed9c22b1e868eae6d37b4a16
msimsik <matej.simsik@yahoo.com>
2021-05-30 20:59:28 +0200

    Interest rate line added

---
+++ PensionGame.Web/Components/InterestRateLine.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace PensionGame.Web.Components
+{
+    public partial class InterestRateLine
+    {
+        [Parameter]
+        public double InterestRate { get; set; }
+    }
+}
