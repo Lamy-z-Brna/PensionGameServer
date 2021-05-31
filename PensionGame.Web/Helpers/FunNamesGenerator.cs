8f3803d0b2d6abb02519eedd4e1bded248ba0217
VladoBaca <asianiajja@gmail.com>
2021-05-31 17:55:39 +0200

    Names generator refactored

---
+++ PensionGame.Web/Helpers/FunNamesGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PensionGame.Web.Helpers
+{
+    public static class FunNamesGenerator
+    {
+        private static readonly Random random = new();
+
+        private static readonly string[] Subjects = { "Shymo", "Anička", "Adam", "Bači", "Miki", "Maja", "Matovič", "Zeman", "Sulík", "Ciťo", "Peťo 2D", "Peťo 3D", "Donald Trump", "Joe Biden", "Babiš", "A scientist", "An epidemologist" };
+        private static readonly string[] Verbs = { "is", "is best in", "ordered", "maintains", "is looking forward to", "loves", "approves of", "is cooking up", "has", "took a photo of", "found a photo of", "doesn't have", "fixed", "created", "found", "knows nothing about", "predicts", "issued a warning about", "waits for", "saves for", "wants", "lost", "won", "won 6 times", "is looking for", "looks like", "caught", "cares about", "would like" };
+        private static readonly string[] Adjectives = { "a huge", "a useless", "an expensive", "a toy", "a not great not terrible", "best in covid", "a crazy", "a dangerous", "an annoying", "no", "an inappropriate", "a cheap", "a fancy", "a tiny", "an overpriced", "a bad", "a significant", "a sleepy", "a malicious", "a stolen", "a massive", "your" };
+        private static readonly string[] Objects = { "vaccine", "lock-down", "pension", "face mask", "Sputnik", "covid", "something", "sense of humour", "bug", "election", "election result", "election fraud", "joke", "result", "victory", "loss", "failure", "character" };
+
+        public static string Generate()
+        {
+            var subjectNoun = PickRandom(Subjects);
+            var verb = PickRandom(Verbs);
+            var adjective = PickRandom(Adjectives);
+            var objectNoun = PickRandom(Objects);
+
+            return $"{subjectNoun} {verb} {adjective} {objectNoun}";
+        }
+
+        private static string PickRandom(string[] collection)
+            => collection[random.Next(0, collection.Length)];
+    }
+}
--- PensionGame.Web/Pages/NewSession.razor.cs
+++ PensionGame.Web/Pages/NewSession.razor.cs
@@ -1,14 +1,14 @@
-﻿using PensionGame.Web.Data;
-using System;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components.Forms;
+﻿using Microsoft.AspNetCore.Components.Forms;
 using PensionGame.Api.Domain.Validation;
+using PensionGame.Web.Data;
+using PensionGame.Web.Helpers;
+using System.Threading.Tasks;
 
 namespace PensionGame.Web.Pages
 {
     public partial class NewSession
     {
-        private NewSessionModel Session { get; } = new(new (30000, 20000, 25, 65), RandName());
+        private NewSessionModel Session { get; } = new(new (500_000, 400_000, 25, 65), FunNamesGenerator.Generate());
 
         private EditContext EditContext { get; set; } = new(new object());
 
@@ -31,22 +31,6 @@ namespace PensionGame.Web.Pages
                     sessionId => NavigationManager.NavigateTo($"/game/{sessionId.Id}"),
                     validationResult => ValidationResult = validationResult
                 );
-        }
-
-        private static string RandName()
-        {
-            string[] names_1 = new string[] { "Shymo", "Anička", "Adam", "Bači", "Miki", "Maja", "Matovič", "Zeman", "Sulík", "Ciťo", "Peťo 2D", "Peťo 3D", "Jesus", "Donald Trump", "Joe Biden", "Babiš", "Your mom", "A scientist", "An epidemologist", "My boss" };
-            string[] names_2 = new string[] { "is", "is best in", "ordered", "maintains", "is looking forward to", "loves", "approves of", "smells like", "is cooking up", "has", "took a photo of", "found a photo of", "doesn't have", "fixed", "created", "found", "knows nothing about", "predicts", "issued a warning about", "waits for", "saves for", "wants", "lost", "won", "won 6 times", "is looking for", "looks like", "caught", "cares about", "doesn't give a fuck about", "would like" };
-            string[] names_3 = new string[] { "a huge", "a useless", "an expensive", "a dirty", "a used", "a toy", "a not great not terrible", "best in covid", "a crazy", "a dangerous", "an overly sexual", "an annoying", "no", "an inappropriate", "a cheap", "a fancy", "a godlike", "a tiny", "an overpriced", "a bad", "a significant", "a sleepy", "a malicious", "a stolen", "a massive", "your" };
-            string[] names_4 = new string[] { "vaccine", "lock-down", "vagina", "pension", "face mask", "Sputnik", "covid", "something", "dick", "bitch", "shit", "sense of humour", "bug", "pussy", "election", "election result", "election fraud", "joke", "result", "mom", "victory", "loss", "failure", "character" };
-
-            Random rand = new();
-            var name_1 = names_1[rand.Next(0, names_1.Length)];
-            var name_2 = names_2[rand.Next(0, names_2.Length)];
-            var name_3 = names_3[rand.Next(0, names_3.Length)];
-            var name_4 = names_4[rand.Next(0, names_4.Length)];
-
-            return $"{name_1} {name_2} {name_3} {name_4}";
         }
 
         protected async void EditContext_OnFieldChanged(object? sender, FieldChangedEventArgs e)
