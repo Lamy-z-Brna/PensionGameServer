dab42697d2a36869c61bf9eb4ce372a410676b2d
msimsik <matej.simsik@yahoo.com>
2021-06-08 22:14:55 +0200

    New game session page + welcome page improvements

--- PensionGame.Web/Pages/Index.razor.cs
+++ PensionGame.Web/Pages/Index.razor.cs
@@ -6,17 +6,5 @@ namespace PensionGame.Web.Pages
 {
     public partial class Index
     {
-        DateTime LastChange;
-        BinaryButton? TradeButton;
-        BinaryButton.Position Old;
-        BinaryButton.Position New;
-
-        private Task Change((BinaryButton.Position, BinaryButton.Position) positions)
-        {
-            LastChange = DateTime.Now;
-            Old = positions.Item1;
-            New = positions.Item2;
-            return Task.CompletedTask;
-        }
     }
 }
