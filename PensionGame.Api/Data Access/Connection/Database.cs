3e21ecd6a88e6002f4ab7d16d7c977e22352b39d
VladoBaca <asianiajja@gmail.com>
2021-06-13 12:22:53 +0200

    timezones fixed

--- PensionGame.Api/Data Access/Connection/Database.cs
+++ PensionGame.Api/Data Access/Connection/Database.cs
@@ -1,5 +1,4 @@
-﻿using MongoDB.Bson;
-using MongoDB.Driver;
+﻿using MongoDB.Driver;
 using PensionGame.Api.Data_Access.ConnectionSettings;
 
 namespace PensionGame.Api.Data_Access.Connection
@@ -10,9 +9,6 @@ namespace PensionGame.Api.Data_Access.Connection
 
         public Database(DatabaseSettings<T> databaseSettings, DatabaseConnectionSettings databaseConnectionSettings)
         {
-#pragma warning disable CS0618 // Type or member is obsolete
-            BsonDefaults.GuidRepresentation = GuidRepresentation.Standard;
-#pragma warning restore CS0618 // Type or member is obsolete
             var client = new MongoClient(databaseConnectionSettings.ConnectionString);
             var database = client.GetDatabase(databaseSettings.DatabaseName);
 
